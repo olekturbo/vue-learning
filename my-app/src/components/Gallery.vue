b9c7582c0f2164ca7d343969a858d8b1d39395fd
Aleksander <aleksander.szewczak@inprox.pl>
2019-03-18 15:40:28 +0100

    Dodanie transition na opis video.

--- my-app/src/components/Gallery.vue
+++ my-app/src/components/Gallery.vue
@@ -2,13 +2,19 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex xs6 offset-xs1>
-        <v-card dark :color="currentSentence % 2 == 0 ? 'rgba(237, 125, 49, 0.7)' : 'rgba(76,76,101, 0.7)'">
-          <v-card-text>
-            {{ list[currentSentence].name }}
-            <br>
-            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac.
-          </v-card-text>
-        </v-card>
+        <transition name="fade">
+          <v-card
+            v-show="show"
+            dark
+            :color="currentSentence % 2 == 0 ? 'rgba(237, 125, 49, 0.7)' : 'rgba(76,76,101, 0.7)'"
+          >
+            <v-card-text>
+              {{ list[currentSentence].name }}
+              <br>
+              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac.
+            </v-card-text>
+          </v-card>
+        </transition>
       </v-flex>
     </v-layout>
   </v-container>
@@ -19,21 +25,22 @@ export default {
   data() {
     return {
       currentSentence: 0,
+      show: true,
       list: [
         {
           name: "Pracuj ciężko",
           body:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac."
         },
         {
           name: "BHP to podstawa",
           body:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac."
         },
         {
           name: "Uważaj na siebie",
           body:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit magna, pellentesque vel libero at, efficitur rhoncus urna. Donec vestibulum ante ipsum, eu varius ipsum aliquam eu. Ut facilisis orci sed vestibulum facilisis. Donec sed malesuada diam. Proin pharetra tincidunt lectus, et efficitur ligula sollicitudin ac."
         }
       ]
     };
@@ -41,7 +48,14 @@ export default {
   mounted() {
     let vm = this;
     setInterval(function() {
-        vm.currentSentence < vm.list.length - 1 ? vm.currentSentence++ : vm.currentSentence = 0;
+      vm.show = false;
+
+      setTimeout(function() {
+        vm.show = true;
+        vm.currentSentence < vm.list.length - 1
+          ? vm.currentSentence++
+          : (vm.currentSentence = 0);
+      }, 1000);
     }, 5000);
   }
 };
@@ -49,6 +63,13 @@ export default {
 
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 .v-stepper {
   background: black !important;
 }
